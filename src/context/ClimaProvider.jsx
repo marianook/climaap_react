87156e6ce4d390a22e2131677adecd2a981f6024
Mariano Hayward <74013144+marianook@users.noreply.github.com>
2023-02-17 15:35:17 -0300

    Fix error

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -26,11 +26,11 @@ const ClimaProvider = ({ children }) => {
     try {
       const { ciudad, pais } = datos;
       const appId = import.meta.env.VITE_API_KEY;
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
       const { data } = await axios(url);
       const { lat, lon } = data[0];
 
-      const urlClima = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
       const { data: clima } = await axios(urlClima);
       setResultado(clima);
     } catch (error) {
